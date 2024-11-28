7cfb41063d6ea2c9029180220d3216d2a82666e1
daniel03f <daniel.fehrenbacher@alumni.fh-aachen.de>
2024-11-28 13:49:37 +0100

    Player Combat & Enemy Interactions
    Player und Enemy Script mit Animations.
    Es gibt nun ein Enemy mit Idle animation.
    Mit "R" kann man das Schwert schwingen.

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour 
+{
+    public Animator animator;
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    // Start is called before the first frame update
+    void Start() {
+        currentHealth = maxHealth;
+    }
+    // Update is called once per frame
+    void Update() {
+        
+    }   
+
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+
+        // Play Hurt Animation
+        Debug.Log("Enemy took " + damage + " damage.");
+        animator.SetTrigger("Hurt");
+
+        if(currentHealth <= 0) {
+            Die();
+        }
+    }
+
+    void Die() {
+        
+        Debug.Log("Enemy died.");
+
+        // Play Death Animation
+        animator.SetBool("IsDead", true);
+
+        // Disable the enemy
+        GetComponent<Collider2D>().enabled = false;  
+        this.enabled = false;
+
+        Destroy(gameObject, 3f);
+    }
+}
---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    public LayerMask enemyLayers;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public int attackDamage = 40;
+    public float attackRate = 2f;
+    private float nextAttackTime = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextAttackTime) 
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }   
+        }
+        
+    }   
+
+
+    // Function to attack enemies in range
+    void Attack()
+    {
+        // Play an Attack Animation
+        animator.SetTrigger("Attack");
+        // Detect enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        // Deal damage to enemies
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+
+        Debug.Log("Attacking");
+    }
+
+
+    // Draw a gizmo to show the attack range
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
