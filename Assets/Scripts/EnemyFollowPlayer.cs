c9003a45802dbeb203c1523ce6ea3ab880990906
daniel03f <daniel.fehrenbacher@alumni.fh-aachen.de>
2024-12-07 22:41:10 +0100

    Deco als PreFab, NavMeshAgent removed, Gegner Folgt Player
    Gegner schwebt noch durch Wände

--- Assets/Scripts/EnemyFollowPlayer.cs
+++ Assets/Scripts/EnemyFollowPlayer.cs
@@ -7,9 +7,11 @@ public class EnemyFollowPlayer : MonoBehaviour
 {
     public Animator animator;
     public Transform player;
-    private NavMeshAgent navMeshAgent;
+    //private NavMeshAgent navMeshAgent;
+    public float speed = 2.0f;
 
     // Start is called before the first frame update
+    /*
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
@@ -23,16 +25,27 @@ public class EnemyFollowPlayer : MonoBehaviour
             Debug.LogError("NavMeshAgent is not placed on a NavMesh on " + gameObject.name);
         }
     }
+    */
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (navMeshAgent != null && navMeshAgent.isOnNavMesh)
         {
             navMeshAgent.SetDestination(player.position);
         }
-    }
+        */
+        if (player != null)
+        {
+            // Richtung zum Spieler berechnen
+            Vector3 direction = (player.position - transform.position).normalized;
 
+            // Bewegung in Richtung des Spielers
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+    /*
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
@@ -48,4 +61,5 @@ public class EnemyFollowPlayer : MonoBehaviour
             navMeshAgent.isStopped = false;
         }
     }
+    */
 }
