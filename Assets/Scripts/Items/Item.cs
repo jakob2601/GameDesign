1627e3ecbaeb6d46396084133d5fd3012b87eb29
Daniel Fehrenbacher <2daniel003@gmail.com>
2025-03-02 17:17:30 +0100

    Add expiration properties to items with configurable time-to-live and canExpire options

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -5,12 +5,21 @@ namespace Scripts.Items
 {
     public abstract class Item : MonoBehaviour
     {
+        [Header("Expire Properties")]
         [SerializeField] protected float timeToLive = 10f; // Time in seconds before the item is destroyed
+        [SerializeField] protected bool canExpire = true; // Whether the item can expire
+        [Header("Item Properties")]
+        [SerializeField] protected int amount = 1; // Amount of item
+        //[SerializeField] protected Audioclip pickupSound; // Sound to play when the item is picked up
+        
 
         protected virtual void Start()
         {
             // Destroy the item after timeToLive seconds
-            Destroy(gameObject, timeToLive);
+            if(canExpire) 
+            {
+                Destroy(gameObject, timeToLive);
+            }
         }
 
         protected abstract void OnTriggerEnter2D(Collider2D collision);
