57d5eecfd74340a637aabc8643b3e1fe762cf2b4
JinrongZhou <117535696+onioncake@users.noreply.github.com>
2025-03-03 15:37:20 +0100

    StartMenu

--- Assets/Scripts/UI/StartMenuController.cs
+++ Assets/Scripts/UI/StartMenuController.cs
@@ -12,6 +12,50 @@ namespace MyGame
             SceneManager.LoadScene("Arena lvl1");
         }
 
+        private void Awake()
+        {
+            // Dieses Objekt beim Szenenwechsel nicht zerstören
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void OnBackClick()
+        {
+            // Überprüfen, ob die Szene bereits geladen ist, um doppeltes Laden zu vermeiden
+            if (SceneManager.GetSceneByName("StartMenu").isLoaded)
+            {
+                Debug.Log("StartMenu ist bereits geladen!");
+                return;
+            }
+
+            // Überprüfen, ob die Szene existiert
+            if (Application.CanStreamedLevelBeLoaded("StartMenu"))
+            {
+                SceneManager.LoadScene("StartMenu", LoadSceneMode.Additive);
+            }
+            else
+            {
+                Debug.LogError("Szene 'StartMenu' existiert nicht in den Build Settings!");
+            }
+        }
+
+        public void OnResumeClick()
+        {
+            // Entfernt das Startmenü, wenn der Spieler zurück ins Spiel möchte
+            Scene scene = SceneManager.GetSceneByName("StartMenu");
+            if (scene.isLoaded)
+            {
+                SceneManager.UnloadSceneAsync("StartMenu");
+            }
+        }
+
+
+
+
+        public void OnControlClick()
+        {
+            SceneManager.LoadScene("ControlsMenu");
+        }
+
         public void OnExitClick()
         {
 #if UNITY_EDITOR
